1c709eb3297bd2efed77c0879dd0cef02ba2ebfc
lwev <106478077+lwev@users.noreply.github.com>
2024-11-12 03:10:21 +0100

    “Update-39”

--- app/components/Bloques.tsx
+++ app/components/Bloques.tsx
@@ -22,6 +22,7 @@ export default function Component() {
   return (
     <div className="bg-gray-800 text-white p-2 min-h-screen rounded-3xl relative">
       <div className="absolute inset-0 z-0 opacity-20">
+        
         <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
           <pattern id="pattern-circles" x="0" y="0" width="50" height="50" patternUnits="userSpaceOnUse" patternContentUnits="userSpaceOnUse">
             <circle id="pattern-circle" cx="10" cy="10" r="1.6257413380501518" fill="#ffffff"></circle>
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -47,6 +47,35 @@ export default function Header() {
   return (
     <header className="text-gray-200 p-6 rounded-xl bg-gray-900 bg-opacity-90 overflow-hidden" style={{ maxWidth: '1000px', margin: '1rem auto' }}>
       <div className="relative">
+      <div className="absolute inset-0 z-0 opacity-20">
+          <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+            <pattern
+              id="pattern-circles"
+              x="0"
+              y="0"
+              width="30"
+              height="40"
+              patternUnits="userSpaceOnUse"
+              patternContentUnits="userSpaceOnUse"
+            >
+              <circle
+                id="pattern-circle"
+                cx="5"
+                cy="5"
+                r="1"
+                fill="#fff"
+              ></circle>
+            </pattern>
+            <rect
+              id="rect"
+              x="0"
+              y="0"
+              width="100%"
+              height="100%"
+              fill="url(#pattern-circles)"
+            ></rect>
+          </svg>
+        </div>
         <motion.div
           initial={{ opacity: 0, scale: 0.8 }}
           animate={{ opacity: 1, scale: 1 }}
