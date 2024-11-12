fcd5a47bd8a7f477545d3db6d18267c73f63180c
lwev <106478077+lwev@users.noreply.github.com>
2024-11-12 02:13:55 +0100

    “Update-35”

--- app/components/Bloques.tsx
+++ app/components/Bloques.tsx
@@ -20,7 +20,7 @@ export default function Component() {
   }
 
   return (
-    <div className="bg-gray-800 text-white p-6 min-h-screen rounded-3xl relative">
+    <div className="bg-gray-800 text-white p-2 min-h-screen rounded-3xl relative">
       <div className="absolute inset-0 z-0 opacity-20">
         <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
           <pattern id="pattern-circles" x="0" y="0" width="50" height="50" patternUnits="userSpaceOnUse" patternContentUnits="userSpaceOnUse">
