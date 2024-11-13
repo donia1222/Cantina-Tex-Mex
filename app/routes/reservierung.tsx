9367b66c1de1968dfee01ac9ce294521b27115d4
lwev <106478077+lwev@users.noreply.github.com>
2024-11-14 00:24:20 +0100

    “Update-54”

---
+++ app/routes/reservierung.tsx
@@ -0,0 +1,10 @@
+import Iframe from "../components/Iframe";
+
+export default function MyPage() {
+  return (
+    <div>
+      <h1>Contenido en Iframe</h1>
+      <Iframe src="https://reservierung.cantinatexmex.ch" />
+    </div>
+  );
+}
