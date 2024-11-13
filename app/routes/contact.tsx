a10edfcfc6090eae045e6da7487dfbaab1fafc5c
lwev <106478077+lwev@users.noreply.github.com>
2024-11-14 00:54:46 +0100

    “Update-55”

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -77,8 +77,8 @@ export default function Contact() {
               <h3 className="text-xl font-semibold mb-4">Reservierung</h3>
               <div className="space-y-4">
                 <motion.a
-                  href="/reservierung"
-                  className="w-full bg-white text-red-600 py-2 px-4 rounded-md shadow-sm text-center font-medium hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition duration-150 ease-in-out flex items-center justify-center"
+                  href="https://reservierung.cantinatexmex.ch"
+                  className=" w-full bg-white text-red-600 py-2 px-4 rounded-md shadow-sm text-center font-medium hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition duration-150 ease-in-out flex items-center justify-center"
                   variants={buttonVariants}
                   whileHover="hover"
                   whileTap="tap"
