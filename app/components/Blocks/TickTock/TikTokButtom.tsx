793c0e9d17e467bb9e6c85afc1e1188f201496d2
lwev <106478077+lwev@users.noreply.github.com>
2024-11-29 16:23:52 +0100

    “ticktock”

--- app/components/Blocks/TickTock/TikTokButtom.tsx
+++ app/components/Blocks/TickTock/TikTokButtom.tsx
@@ -20,7 +20,7 @@ export default function Header() {
         >
           <a
             href="https://www.tiktok.com/@lwebwebsitedesign"
-            className="relative px-6 py-3 font-bold text-gray-100 group max-w-[80%] mx-auto block mt-10"
+            className="relative px-6 py-3 font-bold text-gray-100 group max-w-[80%] mx-auto block"
           >
             <span className="absolute inset-0 transition duration-300 ease-out transform -translate-x-2 -translate-y-2 bg-gray-500 group-hover:translate-x-0 group-hover:translate-y-0"></span>
             <span className="absolute inset-0 border-2 border-gray-300"></span>
