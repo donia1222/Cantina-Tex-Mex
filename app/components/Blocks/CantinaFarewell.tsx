05c7e9139953cc8bedb1ccb3adc0ebb3fb480739
lwev <106478077+lwev@users.noreply.github.com>
2025-03-03 13:48:55 +0100

    “updats”

--- app/components/Blocks/CantinaFarewell.tsx
+++ app/components/Blocks/CantinaFarewell.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function CantinaFarewell() {
   return (
-    <div className=" bg-gradient-to-br from-yellow-200 via-red-300 to-pink-400 flex items-center justify-center p-4">
+    <div className="  flex items-center justify-center p-4">
       <div className="bg-white shadow-lg rounded-lg max-w-3xl p-4 text-center">
         <h1 className="text-3xl font-bold text-red-600 mb-4">
           Liebe Freunde, liebe Gäste
@@ -12,19 +12,7 @@ export default function CantinaFarewell() {
           Nach fast 15 Jahren in Sevelen zieht die Cantina unter neuer Leitung
           nach Buchs.
         </p>
-        <p className="text-lg text-gray-700 leading-relaxed mb-6">
-          Wir, Roberto und Andrea Salvador, haben uns mit der Cantina einen Traum
-          erfüllt und hart dafür gearbeitet, unseren Gästen ein großartiges
-          gastronomisches Erlebnis zu bieten. Wir sind stolz auf das, was wir
-          gemeinsam mit viel Schweiß und Tränen, Optimismus und Engagement
-          erreicht haben. Und wir sind dankbar für die schöne und intensive Zeit
-          in Sevelen.
-        </p>
-        <p className="text-lg text-gray-700 leading-relaxed mb-6">
-          Aber im Leben bleibt nie alles gleich. Seit unserem unfreiwilligen Umzug
-          an die Bahnhofstrasse ist der Cantina Alltag enorm schwierig geworden.
-          Es sind zu viele Steine, die uns den Weg versperren.
-        </p>
+     
         <p className="text-lg text-gray-700 leading-relaxed mb-6">
           Wir freuen uns, dass die Cantina in Buchs ein tolles, neues Zuhause
           gefunden hat. Roberto, das Herz der Cantina, ist weiterhin mit
@@ -34,7 +22,7 @@ export default function CantinaFarewell() {
         <p className="text-lg text-gray-700 leading-relaxed">
           Nun sagen wir also <span className="font-bold">Gracias Sevelen</span> und{" "}
           <span className="font-bold">Adios!</span> <br />
-          <span className="font-bold">Hola Buchs ab März 2025!</span>
+          <span className="font-bold">Hola Buchs ab 20  März 2025!</span>
         </p>
       </div>
     </div>
